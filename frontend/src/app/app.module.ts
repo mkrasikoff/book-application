7f14eb36927d0966cc53e118ea51185a6e8338fb
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-08-10 22:43:43 +0300

    v11 - fixed injection angular error with books page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { BooksComponent } from './books/books.component';
+import { BookService } from "./book.service";
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { BooksComponent } from './books/books.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
