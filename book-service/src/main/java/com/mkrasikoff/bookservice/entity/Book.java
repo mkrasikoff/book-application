778855ac688da72fa170f42c31b6880b8191c184
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-08-29 23:08:42 +0300

    v22 - fixed auto generated value types for Hibernate

--- book-service/src/main/java/com/mkrasikoff/bookservice/entity/Book.java
+++ book-service/src/main/java/com/mkrasikoff/bookservice/entity/Book.java
@@ -18,7 +18,7 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @Column(nullable = false)
     private String title;
