2e1f54c2f10f9b292bf9bd13bc097ada03a0ff2a
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-09-16 10:54:36 +0300

    v36 - added documentation for UserRepository

--- user-service/src/main/java/com/mkrasikoff/userservice/repo/UserRepository.java
+++ user-service/src/main/java/com/mkrasikoff/userservice/repo/UserRepository.java
@@ -5,8 +5,21 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+/**
+ * UserRepository interface that extends the CrudRepository.
+ * This provides CRUD functionality for the User entity.
+ */
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    /**
+     * Finds the active user in the database.
+     *
+     * This method will return an Optional containing the User entity that has
+     * its 'isActive' field set to true. If no active user is found,
+     * it returns an empty Optional.
+     *
+     * @return an Optional containing the active User or an empty Optional if not found.
+     */
     Optional<User> findByIsActiveTrue();
 }
