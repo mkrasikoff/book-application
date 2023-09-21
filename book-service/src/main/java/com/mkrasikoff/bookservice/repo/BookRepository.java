05094e6ab6b27e6ebe22c558a3b09b52e247c3da
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-09-21 22:24:57 +0300

    v40 - added book repo documentation

--- book-service/src/main/java/com/mkrasikoff/bookservice/repo/BookRepository.java
+++ book-service/src/main/java/com/mkrasikoff/bookservice/repo/BookRepository.java
@@ -4,6 +4,18 @@ import com.mkrasikoff.bookservice.entity.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The BookRepository interface provides CRUD (Create, Read, Update, Delete)
+ * functionalities for the Book entity by extending the CrudRepository
+ * interface provided by Spring Data JPA.
+ *
+ * By extending CrudRepository, it leverages default database operations
+ * such as save, delete, findById, and many others without the need for custom
+ * implementations.
+ *
+ * Use this repository interface to perform database operations related to the
+ * Book entity.
+ */
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 
