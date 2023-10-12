c8770d4d8fd78f958ccfe3e8aa01ba66a0a0951d
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-10-12 22:10:40 +0300

    v56 - kafka config and service added, new properties, kafka dependency

---
+++ user-service/src/main/java/com/mkrasikoff/userservice/service/UserCreatedProducer.java
@@ -0,0 +1,24 @@
+package com.mkrasikoff.userservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserCreatedProducer {
+
+    @Value("${kafka.topic.userCreated}")
+    private String userCreatedTopic;
+
+    private final KafkaTemplate<String, Long> kafkaTemplate;
+
+    @Autowired
+    public UserCreatedProducer(KafkaTemplate<String, Long> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendUserId(Long userId) {
+        kafkaTemplate.send(userCreatedTopic, userId);
+    }
+}
