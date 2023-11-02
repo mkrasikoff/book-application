c763b34216e7c83a859d0f048a958752467395cf
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-11-02 22:34:50 +0200

    v70 - added service tests to file structure

---
+++ book-service/src/test/java/com/mkrasikoff/bookservice/service/AuthorServiceTest.java
@@ -0,0 +1,28 @@
+package com.mkrasikoff.bookservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getByNameAndSurname() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
---
+++ book-service/src/test/java/com/mkrasikoff/bookservice/service/BookServiceTest.java
@@ -0,0 +1,32 @@
+package com.mkrasikoff.bookservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAllByUserId() {
+    }
+
+    @Test
+    void deleteAllBooksByUserId() {
+    }
+}
