5f295f8f6f53669a42d149084548bba6b502d7e9
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-11-03 12:52:53 +0200

    v71 - added tests to BookServiceTest

--- book-service/src/test/java/com/mkrasikoff/bookservice/service/BookServiceTest.java
+++ book-service/src/test/java/com/mkrasikoff/bookservice/service/BookServiceTest.java
@@ -1,32 +1,96 @@
 package com.mkrasikoff.bookservice.service;
 
+import com.mkrasikoff.bookservice.entity.Book;
+import com.mkrasikoff.bookservice.repo.AuthorRepository;
+import com.mkrasikoff.bookservice.repo.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class BookServiceTest {
 
-    @Test
-    void save() {
+    @InjectMocks
+    private BookServiceImpl bookService;
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    void update() {
+    void save_whenCorrectBookIsGiven_thenBookWasSaved() {
+        Book book = new Book();
+        when(bookRepository.save(any())).thenReturn(book);
+
+        Book result = bookService.save(book, 1L);
+
+        assertEquals(book, result);
     }
 
     @Test
-    void get() {
+    void update_whenNewDataAboutBookIsGiven_thenBookWasUpdated() {
+        Book book = new Book();
+        Book newBook = new Book();
+        when(bookRepository.findById(any())).thenReturn(Optional.of(book));
+        when(bookRepository.save(any())).thenReturn(book);
+
+        Book result = bookService.update(1L, newBook);
+
+        assertEquals(book, result);
     }
 
     @Test
-    void delete() {
+    void update_whenBookDoesNotExist_thenExceptionWasThrown() {
+        when(bookRepository.findById(any())).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> bookService.update(1L, new Book()));
     }
 
     @Test
-    void getAllByUserId() {
+    void get_whenBookExists_thenBookWasReturned() {
+        Book book = new Book();
+        when(bookRepository.findById(any())).thenReturn(Optional.of(book));
+
+        Book result = bookService.get(1L);
+
+        assertEquals(book, result);
     }
 
     @Test
-    void deleteAllBooksByUserId() {
+    void get_whenBookDoesNotExist_thenExceptionWasThrown() {
+        when(bookRepository.findById(any())).thenReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> bookService.get(1L));
+    }
+
+    @Test
+    void delete_whenBookExists_thenBookWasDeleted() {
+        when(bookRepository.existsById(any())).thenReturn(true);
+
+        bookService.delete(1L);
+
+        verify(bookRepository).deleteById(1L);
+    }
+
+    @Test
+    void delete_whenBookDoesExist_thenExceptionWasThrown() {
+        when(bookRepository.existsById(any())).thenReturn(false);
+
+        assertThrows(ResponseStatusException.class, () -> bookService.delete(1L));
     }
 }
