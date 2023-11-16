494c48a7eb89ea50b074c7602c097d903649ab66
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-11-16 22:51:13 +0200

    v78 - added logging to AuthorService

--- book-service/src/main/java/com/mkrasikoff/bookservice/service/AuthorServiceImpl.java
+++ book-service/src/main/java/com/mkrasikoff/bookservice/service/AuthorServiceImpl.java
@@ -3,6 +3,8 @@ package com.mkrasikoff.bookservice.service;
 import com.mkrasikoff.bookservice.entity.Author;
 import com.mkrasikoff.bookservice.repo.AuthorRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,36 +15,42 @@ import java.util.Optional;
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository authorRepository;
+    private static final Logger log = LoggerFactory.getLogger(AuthorServiceImpl.class);
 
     @Override
     public Author save(Author author) {
         Optional<Author> existingAuthor = authorRepository.findByNameAndSurname(author.getName(), author.getSurname());
         if (existingAuthor.isPresent()) {
-            // add logging in the future
+            log.info("Attempt to save duplicate author: '{}' '{}'", author.getName(), author.getSurname());
             return existingAuthor.get();
-        } else {
-            return authorRepository.save(author);
         }
+        return authorRepository.save(author);
     }
 
     @Override
     public Author get(Long id) {
-        return authorRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found"));
+        return authorRepository.findById(id).orElseThrow(() -> {
+                    log.error("Author with ID '{}' not found", id);
+                    return new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found");
+                });
     }
 
     @Override
     public Optional<Author> getByNameAndSurname(String name, String surname) {
-        return authorRepository.findByNameAndSurname(name, surname);
+        Optional<Author> authorOptional = authorRepository.findByNameAndSurname(name, surname);
+        if (!authorOptional.isPresent()) {
+            log.info("Author with name: '{}' and surname: '{}' not found", name, surname);
+        }
+        return authorOptional;
     }
 
     @Override
     public void delete(Long id) {
-        if (authorRepository.existsById(id)) {
-            authorRepository.deleteById(id);
-        } else {
+        if (!authorRepository.existsById(id)) {
+            log.error("Attempt to delete non-existent author with ID '{}'", id);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found");
         }
+        authorRepository.deleteById(id);
     }
 
     @Override
@@ -51,6 +59,9 @@ public class AuthorServiceImpl implements AuthorService {
             author.setName(newAuthor.getName());
             author.setSurname(newAuthor.getSurname());
             return authorRepository.save(author);
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found"));
+        }).orElseThrow(() -> {
+            log.error("Attempt to update non-existent author with ID '{}'", id);
+            return new ResponseStatusException(HttpStatus.NOT_FOUND, "Author not found");
+        });
     }
 }
