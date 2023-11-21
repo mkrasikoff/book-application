85eef39e16dd6a601f7875b2109769fa30f05b84
Mikhail Krasikov <mskrasikoff@gmail.com>
2023-11-21 23:45:14 +0200

    v85 - fixed UserCreatedProducer test failure

--- user-service/src/test/java/com/mkrasikoff/bookservice/producer/UserCreatedProducerTest.java
+++ user-service/src/test/java/com/mkrasikoff/bookservice/producer/UserCreatedProducerTest.java
@@ -7,8 +7,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.SettableListenableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class UserCreatedProducerTest {
 
@@ -24,12 +29,21 @@ class UserCreatedProducerTest {
     }
 
     @Test
-    void sendUserId_whenCalled_thenSendsMessageToKafka() {
+    void sendUserId_whenCalled_thenSendsMessageToKafka() throws InterruptedException {
         Long userId = 1L;
         String userCreatedTopic = "user-created-topic";
+        SettableListenableFuture<SendResult<String, Long>> future = new SettableListenableFuture<>();
+        CountDownLatch latch = new CountDownLatch(1);
+        when(kafkaTemplate.send(eq(userCreatedTopic), eq(userId))).thenReturn(future);
 
         userCreatedProducer.sendUserId(userId);
 
         verify(kafkaTemplate).send(eq(userCreatedTopic), eq(userId));
+        // Simulate a successful send
+        SendResult<String, Long> sendResult = new SendResult<>(null, null);
+        future.set(sendResult);
+        future.addCallback(result -> latch.countDown(), ex -> latch.countDown());
+        boolean await = latch.await(1, TimeUnit.SECONDS);
+        assert await;
     }
 }
